5f02bc0fcbe3276bf6b6dd35aef210be2d7ce502
JanHolger <jan@bebendorf.eu>
2021-05-19 22:24:19 +0200

    Added missing status

--- src/main/java/eu/bebendorf/tswebquery/model/ServerStatus.java
+++ src/main/java/eu/bebendorf/tswebquery/model/ServerStatus.java
@@ -1,14 +1,13 @@
 package eu.bebendorf.tswebquery.model;
 
-import com.sun.org.apache.xerces.internal.util.Status;
-
 public enum ServerStatus {
     ONLINE,
+    ONLINE_VIRTUAL,
     OFFLINE,
     UNKNOWN;
     public static ServerStatus fromString(String status){
         for(ServerStatus s : values()){
-            if(s.name().equalsIgnoreCase(status)){
+            if(s.name().replace('_', ' ').equalsIgnoreCase(status)){
                 return s;
             }
         }
